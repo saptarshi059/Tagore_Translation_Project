549d216dd23638f4600f45c52a8527328753d989
Saptarshi <ssengupta8@gmail.com>
2026-05-31 18:23:39 -0400

    inference script

--- src/model_training.py
+++ src/model_training.py
@@ -31,7 +31,7 @@ def main(model_name):
         gradient_accumulation_steps=8,
         learning_rate=4e-5,
         logging_steps=5,
-        assistant_only_loss=False,
+        assistant_only_loss=True,
         gradient_checkpointing=True,
     )
 
