d511ae0d4dac0dbc2f974063475b36bda21c3737
Saptarshi <ssengupta8@gmail.com>
2026-06-01 17:07:32 -0400

    changing training target

--- src/model_training.py
+++ src/model_training.py
@@ -29,7 +29,7 @@ def main(model_name):
         output_dir="./bn_en_model",
         per_device_train_batch_size=1,
         gradient_accumulation_steps=8,
-        learning_rate=4e-5,
+        learning_rate=2e-4,
         logging_steps=5,
         assistant_only_loss=False,
         gradient_checkpointing=True,
