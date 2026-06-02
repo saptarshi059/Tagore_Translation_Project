5fda8bd72b5dec0929f03dfe52b06ffad2090ed9
Saptarshi <ssengupta8@gmail.com>
2026-06-01 20:06:26 -0400

    trying simple translation

--- src/model_training.py
+++ src/model_training.py
@@ -27,7 +27,7 @@ def main(model_name):
     training_args = SFTConfig(
         output_dir="./bn_en_model",
         per_device_train_batch_size=1,
-        gradient_accumulation_steps=8,
+        gradient_accumulation_steps=4,
         learning_rate=1e-5,
         logging_steps=5,
         assistant_only_loss=False,
