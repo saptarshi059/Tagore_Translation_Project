7e10a0b0dfd0c8f9641bd7cdc52f3e1208cd4508
Saptarshi <ssengupta8@gmail.com>
2026-06-08 17:31:46 -0400

    Final Phase - moving to DPO

--- src/DPO_code/train.py
+++ src/DPO_code/train.py
@@ -1,4 +1,9 @@
-from trl import DPOTrainer
+import os
+os.environ['TOKENIZERS_PARALLELISM'] = "false"
+os.environ['CUDA_VISIBLE_DEVICES']= "0"
+
+from transformers import AutoModelForCausalLM
+from trl import DPOTrainer, DPOConfig
 from datasets import load_dataset
 import sys
 sys.path.append("../../src/common")
@@ -15,8 +20,34 @@ def preprocess_function(example):
 
 def main():
     dataset = load_dataset("parquet", data_files="../../data/DPO_data/parsed_preferences.parquet", split='train')
+
+    print('Processing dataset...')
     dataset = dataset.map(preprocess_function, remove_columns=dataset.column_names)
-    print(next(iter(dataset)))
+
+    print('Loading SFT checkpoint...')
+    model = AutoModelForCausalLM.from_pretrained('../SFT_code/bn_en_model/', device_map='auto')
+
+    training_args = DPOConfig(
+        output_dir="./bn_en_model_DPO",
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=4,
+        learning_rate=1e-5,
+        logging_steps=5,
+        gradient_checkpointing=True,
+    )
+
+    trainer = DPOTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset,
+    )
+
+    print("Starting training...")
+    trainer.train()
+
+    print('Training done, saving model...')
+    trainer.save_model()
+
 
 if __name__ == "__main__":
     main()
